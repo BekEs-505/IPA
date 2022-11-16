3ed499e5943bae9439772934e5e07c490fb5f04e
BekEs-505 <snap.b6.7n@gmail.comgit config -lgit config --global user.email  snap.b6.7n@gmail.com>
2022-11-16 21:30:47 +0300

    first commit

--- www.ammaripa.com/style.js
+++
@@ -1,1 +0,0 @@
-function dw(data) {$.ajax({url: "admin/ajax.php",data: {downloadApp: data,ajax: 5}});}function download(id){$.ajax({url: "admin/ajax.php",data: {ajax: 3,id: id, }, dataType: "json",success: (res) => {window.location.href = res.url;}});}function er() {alert("عذراً التحميل من الايفون والايباد فقط");}
